0e16cb0647fad74ce26953e11453c81f535c4b46
Santiago <santiago.morales@utec.edu.pe>
2026-04-25 14:08:42 -0500

    cambios

--- src/main/java/org/e2e/labe2e01/userLocations/domain/UserLocation.java
+++ src/main/java/org/e2e/labe2e01/userLocations/domain/UserLocation.java
@@ -3,12 +3,16 @@ package org.e2e.labe2e01.userLocations.domain;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.e2e.labe2e01.coordinate.domain.Coordinate;
 import org.e2e.labe2e01.passenger.domain.Passenger;
 
 @Entity
 @NoArgsConstructor
+@Setter
+@Getter
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class UserLocation {
     @EmbeddedId
@@ -24,6 +28,8 @@ public class UserLocation {
     @MapsId("coordinateId")
     private Coordinate coordinate;
 
+    private String description;
+
     public UserLocation(Passenger passenger, Coordinate coordinate, String description) {
         this.passenger = passenger;
         this.coordinate = coordinate;
@@ -31,4 +37,5 @@ public class UserLocation {
         this.id = new PassengerCoordinateId(passenger.getId(), coordinate.getId());
     }
 }
+
 
--- src/main/java/org/e2e/labe2e01/vehicle/domain/Vehicle.java
+++ src/main/java/org/e2e/labe2e01/vehicle/domain/Vehicle.java
@@ -1,10 +1,34 @@
 package org.e2e.labe2e01.vehicle.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import jakarta.persistence.Entity;
+import lombok.Setter;
+import org.e2e.labe2e01.driver.domain.Driver;
 
 @Entity
 @NoArgsConstructor
+@Setter
+@Getter
 public class Vehicle {
+    @Id
+    private Long id;
+
+    @Column(unique = true, nullable = false,columnDefinition = "VARCHAR(255)")
+    private String licensePlate;
+
+    @Column(nullable = false)
+    private Integer capacity;
+
+    @Column(nullable = false)
+    private Integer fabricationYear;
+
+    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+    private String brand;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(255)")
+    private String model;
+
+    @OneToOne
+    private Driver conductor;
 }
